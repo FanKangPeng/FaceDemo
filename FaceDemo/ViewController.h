8f62dc9430787a859193283ec4ec1ed18c2bd6e0
FanKing <15936219505@163.com>
2016-03-03 11:05:29 +0800

    Initial Commit

---
+++ FaceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceDemo
+//
+//  Created by 樊康鹏 on 16/3/3.
+//  Copyright © 2016年 FanKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FaceDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FaceDemo
+//
+//  Created by 樊康鹏 on 16/3/3.
+//  Copyright © 2016年 FanKing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
